aea92193addc76a8611269adc039d75b4dd27c8c
tade-art <t8dukas@gmail.com>
2026-03-03 19:38:58 +0000

    Initial Commit

---
+++ src/main/java/com/tade/secure_file_api/SecureFileApiApplication.java
@@ -0,0 +1,13 @@
+package com.tade.secure_file_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFileApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFileApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tade/secure_file_api/SecureFileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tade.secure_file_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureFileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
